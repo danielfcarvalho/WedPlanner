ae5987fcf829bf46aad2e7a1e08abd88d7ea487b
afarturc <art.afo@ua.pt>
2022-05-18 00:56:07 +0100

    Login

--- wed-planner/src/Login/index.js
+++ wed-planner/src/Login/index.js
@@ -3,17 +3,23 @@
 function Login() {
     return (
         <div>
-            <h1> LOGIN </h1>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <h1 style={{textAlign: 'center'}}> LOGIN </h1>
+            <div className="row mb-4 justify-content-center">
+            <div className="col-8">
+              <select className="form-select" aria-label="Default select example">
+                <option defaultValue>username</option>
+                <option value="1">Monica(Admin Account)</option>
+                <option value="2">Rachel(Helper Account)</option>
+              </select>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3">Confirm identity</button>
+            <div className="row mb-4 justify-content-center">
+              <div className="col-8">
+                <input type="password" className="form-control" id="inputPassword"/>
+              </div>
+            </div>
+            <div className="row justify-content-center">
+                <button type="submit" className="btn btn-secondary mb-3 col-4">Login</button>
             </div>
         </div>
     );
